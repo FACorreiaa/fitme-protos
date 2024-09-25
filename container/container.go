26ff78772b87642786eae407c7868ed5642991a4
FACorreiaa <fernandocorreia316@gmail.com>
2024-09-25 16:50:48 +0100

    make calculator proto

--- container/container.go
+++ container/container.go
@@ -25,16 +25,17 @@ type Brokers struct {
 }
 
 // NewBrokers creates a common container instance for use with all cluster sevices
-func NewBrokers(transportUtils *utils.TransportUtils) *Brokers {
+func NewBrokers(transportUtils *utils.TransportUtils, auth *user.AuthClient, customer *customer.CustomerClient,
+	calc *calculator.CalculatorServiceClient) *Brokers {
 	if transportUtils == nil {
 		return nil
 	}
 
 	brokers := new(Brokers)
 	brokers.TransportUtils = transportUtils
-	//brokers.Customer = customer
-	//brokers.Auth = auth
-	//brokers.Calculator = calc
+	brokers.Customer = customer
+	brokers.Auth = auth
+	brokers.Calculator = calc
 	utils.Transport = transportUtils
 
 	return brokers
